65ab6f2e043e40e47843184bd868b2ac53e19b86
Stuart Thomson <stu.thom.96@gmail.com>
2017-06-17 11:42:36 +1200

    Remove LaunchUriActivity UI

--- app/src/main/java/kiwi/sthom/mars/LaunchUriActivity.java
+++ app/src/main/java/kiwi/sthom/mars/LaunchUriActivity.java
@@ -1,13 +1,9 @@
 package kiwi.sthom.mars;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 import com.microsoft.connecteddevices.ConnectedDevicesException;
@@ -16,7 +12,7 @@ import com.microsoft.connecteddevices.RemoteLauncher;
 import com.microsoft.connecteddevices.RemoteSystem;
 import com.microsoft.connecteddevices.RemoteSystemConnectionRequest;
 
-public class LaunchUriActivity extends AppCompatActivity {
+public class LaunchUriActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,11 +38,26 @@ public class LaunchUriActivity extends AppCompatActivity {
                             .makeText(this, message, Toast.LENGTH_LONG)
                             .show();
                     });
+                } else {
+                    runOnUiThread(() -> {
+                        String message = getString(R.string.send_error, device.getDisplayName());
+                        Toast
+                            .makeText(this, message, Toast.LENGTH_LONG)
+                            .show();
+                    });
                 }
+
+                finish();
             });
         } catch (ConnectedDevicesException ex) {
             Log.e("CDE", ex.getLocalizedMessage());
         }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        finish();
+    }
 }
