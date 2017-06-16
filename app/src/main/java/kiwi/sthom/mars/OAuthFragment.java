00f2a8ead477326379b58eb0f14fe3d33d6be38a
Stuart Thomson <stu.thom.96@gmail.com>
2017-06-16 18:10:53 +1200

    Add implementation to OAuth fragment
    Much help from MS's own docs.
    https://github.com/Microsoft/project-rome/blob/master/Android/how-to%20guides/getting-started-rome-android.md

--- app/src/main/java/kiwi/sthom/mars/OAuthFragment.java
+++ app/src/main/java/kiwi/sthom/mars/OAuthFragment.java
@@ -7,56 +7,52 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link OAuthFragment.OnFragmentInteractionListener} interface
+ * {@link OnOAuthCodeListener} interface
  * to handle interaction events.
  * Use the {@link OAuthFragment#newInstance} factory method to
  * create an instance of this fragment.
- *
  */
 public class OAuthFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String REDIRECT_URI = "https://login.live.com/oauth20_desktop.srf";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String ARG_OAUTH_URL = "oauth-url";
 
-    private OnFragmentInteractionListener mListener;
+    private String _oauthUrl;
+
+    private OnOAuthCodeListener _listener;
+
+    public OAuthFragment() {
+        // Required empty public constructor
+    }
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param url URL to load initially
      * @return A new instance of fragment OAuthFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static OAuthFragment newInstance(String param1, String param2) {
+    public static OAuthFragment newInstance(String url) {
         OAuthFragment fragment = new OAuthFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_OAUTH_URL, url);
         fragment.setArguments(args);
         return fragment;
-    }
-    public OAuthFragment() {
-        // Required empty public constructor
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            _oauthUrl = getArguments().getString(ARG_OAUTH_URL);
         }
     }
 
@@ -64,31 +60,66 @@ public class OAuthFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_oauth, container, false);
+        View v = inflater.inflate(R.layout.fragment_oauth, container, false);
+
+        WebView wv = v.findViewById(R.id.oauth_webview);
+        wv.setWebChromeClient(new WebChromeClient());
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.getSettings().setDomStorageEnabled(true);
+
+        // Load OAuth URL
+        WebViewClient client = new WebViewClient() {
+            // Make sure we don't send the auth code out twice
+            boolean done = false;
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                if (url.startsWith(REDIRECT_URI)) {
+                    // extract the auth code from the url
+                    Uri uri = Uri.parse(url);
+                    String code = uri.getQueryParameter("code");
+                    String error = uri.getQueryParameter("error");
+
+                    if (code != null && !done) {
+                        done = true;
+                        onAuthCodeFetched(code);
+                    } else if (error != null) {
+                        // Handle error case
+                        // For now, just log
+                    }
+                }
+            }
+        };
+        wv.setWebViewClient(client);
+
+        wv.loadUrl(_oauthUrl);
+
+        return v;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    public void onAuthCodeFetched(String code) {
+        if (_listener != null) {
+            _listener.onOAuthCode(code);
         }
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof OnOAuthCodeListener) {
+            _listener = (OnOAuthCodeListener) context;
         } else {
             throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+                + " must implement OnOAuthCodeListener");
         }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+        _listener = null;
     }
 
     /**
@@ -96,13 +127,8 @@ public class OAuthFragment extends Fragment {
      * fragment to allow an interaction in this fragment to be communicated
      * to the activity and potentially other fragments contained in that
      * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    public interface OnOAuthCodeListener {
+        void onOAuthCode(String string);
     }
 }
