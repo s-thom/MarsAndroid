dbb3f7a7d5c6b3aa143364771c1fe81756dac63a
Stuart Thomson <stu.thom.96@gmail.com>
2017-06-17 11:33:34 +1200

    Send shared URL
    There we go. It works. Somehow, I managed to make it work.

--- app/src/main/java/kiwi/sthom/mars/DeviceChooserService.java
+++ app/src/main/java/kiwi/sthom/mars/DeviceChooserService.java
@@ -1,12 +1,9 @@
 package kiwi.sthom.mars;
 
-import android.app.Service;
 import android.content.ComponentName;
-import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.drawable.Icon;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.service.chooser.ChooserTarget;
 import android.service.chooser.ChooserTargetService;
 import android.util.Log;
@@ -15,7 +12,6 @@ import com.microsoft.connecteddevices.RemoteSystem;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class DeviceChooserService extends ChooserTargetService {
     static final String KEY_DEVICE_ID = "kiwi.sthom.mars.DEVICE_ID";
--- app/src/main/java/kiwi/sthom/mars/LaunchUriActivity.java
+++ app/src/main/java/kiwi/sthom/mars/LaunchUriActivity.java
@@ -8,7 +8,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.microsoft.connecteddevices.ConnectedDevicesException;
+import com.microsoft.connecteddevices.RemoteLaunchUriStatus;
+import com.microsoft.connecteddevices.RemoteLauncher;
 import com.microsoft.connecteddevices.RemoteSystem;
 import com.microsoft.connecteddevices.RemoteSystemConnectionRequest;
 
@@ -21,6 +25,27 @@ public class LaunchUriActivity extends AppCompatActivity {
         if (i.getExtras() == null) {
             Log.d("URI", "No extras found");
             finish();
+        }
+
+        RemoteSystem device = DeviceStorage.getDevice(i.getStringExtra(DeviceChooserService.KEY_DEVICE_ID));
+        String url = i.getStringExtra(Intent.EXTRA_TEXT);
+        String pageName = i.getStringExtra(Intent.EXTRA_SUBJECT);
+
+        RemoteSystemConnectionRequest connReq = new RemoteSystemConnectionRequest(device);
+
+        try {
+            RemoteLauncher.LaunchUriAsync(connReq, url, status -> {
+                if (status == RemoteLaunchUriStatus.SUCCESS) {
+                    runOnUiThread(() -> {
+                        String message = getString(R.string.send_success, pageName, device.getDisplayName());
+                        Toast
+                            .makeText(this, message, Toast.LENGTH_LONG)
+                            .show();
+                    });
+                }
+            });
+        } catch (ConnectedDevicesException ex) {
+            Log.e("CDE", ex.getLocalizedMessage());
         }
     }
 
