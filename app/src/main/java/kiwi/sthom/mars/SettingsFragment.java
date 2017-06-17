e8e52fb940d2deafbe23a80b936ed6ee893c7366
Stuart Thomson <stu.thom.96@gmail.com>
2017-06-17 21:25:58 +1200

    Show dialog when sign out is selected

--- app/src/main/java/kiwi/sthom/mars/SettingsFragment.java
+++ app/src/main/java/kiwi/sthom/mars/SettingsFragment.java
@@ -1,9 +1,16 @@
 package kiwi.sthom.mars;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,4 +44,29 @@ public class SettingsFragment extends PreferenceFragment {
         addPreferencesFromResource(R.xml.preferences);
     }
 
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        switch (preference.getKey()) {
+            case "prefs_sign_out":
+                new AlertDialog.Builder(getContext())
+                    .setTitle(R.string.prefs_sing_out_dialog_title)
+                    .setMessage(R.string.prefs_sign_out_dialog_content)
+                    .setPositiveButton(R.string.prefs_sign_out_dialog_action, (DialogInterface dialogInterface, int i) -> {
+                        Uri uri = Uri.fromParts("package", getContext().getPackageName(), null);
+                        Intent intent = new Intent()
+                            .setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                            .setData(uri);
+                        startActivity(intent);
+                    })
+                    .setNegativeButton(R.string.prefs_sign_out_dialog_cancel, (DialogInterface dialogInterface, int i) -> {})
+                    .setCancelable(true)
+                    .create()
+                    .show();
+                return true;
+            default:
+                return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+
+
+    }
 }
