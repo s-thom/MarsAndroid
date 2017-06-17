59ebd4287cd3e81f6a12dcca94e043925b7752d7
Stuart Thomson <stu.thom.96@gmail.com>
2017-06-17 20:47:52 +1200

    Create SettingsFragment

---
+++ app/src/main/java/kiwi/sthom/mars/SettingsFragment.java
@@ -0,0 +1,45 @@
+package kiwi.sthom.mars;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SettingsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SettingsFragment extends PreferenceFragment {
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment SettingsFragment.
+     */
+    public static SettingsFragment newInstance() {
+        SettingsFragment fragment = new SettingsFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+}
